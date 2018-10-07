e87313686cb6a8a47e621a341b1cbd0d3f7ba36e
NiciusB <nuno@balbona.me>
2018-10-07 10:48:05 +0000

    Using new DM twitter API

--- index.js
+++ index.js
@@ -17,23 +17,25 @@ const statuses = [
 
 function interval() {
     const mainClient = botPool.getClient(0)
-    mainClient.get('direct_messages', { count: 200 }).then(result => {
-        if (result.data && typeof result.data.forEach === 'function') {
-            result.data.forEach(message => {
+
+    mainClient.get('direct_messages/events/list', { count: 50 }).then(result => {
+        if (result.data && result.data.events && typeof result.data.events.forEach === 'function') {
+            result.data.events.forEach(msg_event => {
+		const message = msg_event.message_create.message_data
                 var removeDirectMessage = true
                 if (message.entities.user_mentions.length) {
                     removeDirectMessage = false
                     message.entities.user_mentions.forEach(mention => {
                         var tweet = botPool.tweet({ status: '@' + mention.screen_name + ' ' + statuses[Math.floor(Math.random() * statuses.length)] })
-                        if (tweet) {
+                        if ( tweet) {
                             tweet.promise.then(res => {
-                                if (!res.data.entities || !res.data.entities.user_mentions.length) tweet.client.delete('direct_messages/destroy', { id: res.data.id_str })
+                                if (!res.data.entities || !res.data.entities.user_mentions.length) tweet.client.post('statuses/destroy/:id', { id: res.data.id_str })
                             })
                             removeDirectMessage = true
                         }
                     })
                 }
-                if (removeDirectMessage) mainClient.delete('direct_messages/destroy', { id: message.id_str })
+                if (removeDirectMessage) mainClient.delete('direct_messages/events/destroy', { id: msg_event.id })
             })
         }
     })
